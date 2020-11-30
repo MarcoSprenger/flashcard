8268de765c102c0fa2745484d57f116add425eef
Sprenger Marco <msprenger99@gmx.ch>
2020-11-30 16:41:14 +0100

    Allowed CORS for REST-API

--- flashcard-rest/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ flashcard-rest/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ch.fhnw.webfr.flashcard.domain.Questionnaire;
 import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/questionnaires")
 public class QuestionnaireController {
